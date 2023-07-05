f7cd77e4fc8eedfbfe1625176949bdda398bb0ca
kipu1 <israel.ortiz.est@tecazuay.edu.ec>
2023-07-05 16:39:07 -0500

    venta

--- src/app/detalleorden/detalleorden.ts
+++ src/app/detalleorden/detalleorden.ts
@@ -1,10 +1,10 @@
 export class detalleorden {
    
-    cedula: 'string';
-    nombre: 'string';
-    servicio: 'string';
-    maquina: number=0;
-    fecha: Date ;
-    peso: number=0;
-    total: number=0;
+    indentifiacion:string;
+    nombre:string;
+    servicio:string;
+    maquina:number=0;
+    fecha:Date ;
+    peso:number=0;
+    total:number=0;
 }
