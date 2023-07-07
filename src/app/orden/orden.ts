684038dae5079e9348efdc918cfd583ff9f9c6b3
Marcos Pillco <marcospillcocontreras@gmail.com>
2023-07-06 22:58:47 -0500

    Ordenes cambios

--- src/app/orden/orden.ts
+++ src/app/orden/orden.ts
@@ -1,16 +1,8 @@
 export class orden {
-   
-<<<<<<< HEAD
-    id_orden:number=0;
-    estado:number=0;
-    total_orden:number=0;
-    id_factura:number=0;
-    id_persona:number=0;
-=======
     idOrden:number=0;
     estado:number=0;
     totalOrden:number=0;
-    venta:number=0;
-    personaO:number=0;
->>>>>>> 15da59f0240c2404c09067148730fc01d2f2a128
+    idVenta:number=0;
+    idPersona:number=0;
+
 }
