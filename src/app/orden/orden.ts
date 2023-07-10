5da9a264cb10e9a3f48a1a7237d8da045c56c666
Josvich <josue.suarez.est@tecazuay.edu.ec>
2023-07-10 07:44:26 -0500

    Cambios Josviche-Marcos
    Cambios Definitos Ordenes

--- src/app/orden/orden.ts
+++ src/app/orden/orden.ts
@@ -1,21 +1,9 @@
 import { Persona } from "../modelo/persona";
 import { venta } from "../venta/venta";
-<<<<<<< HEAD
-
 export class orden {
     idOrden:number;
     estado:number;
     totalOrden:number;
     venta:venta;
     personaO:Persona;
-=======
->>>>>>> 9c03431d673c0813bcb66f62a88fd37b8e18f8cb
-
-export class orden {
-    idOrden: number;
-   estado: number;
-   totalOrden: number;
-   venta: venta;
-   personaO: Persona;
 }
-
