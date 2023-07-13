926a139ec3e1bc0b79591b00080e313c0dc59841
CAB3ZON <marcospillcocontreras@gmail.com>
2023-07-12 22:18:20 -0500

    pantalla_empleado
    pantalla_empleado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,7 +42,7 @@ const routes:Routes = [
   { path: 'ventas', component: VentasComponent },
   { path: 'config-empresa', component: ConfigEmpresaComponent},
   { path: 'actualizar-empresa/:id', component:ActualizarEmpresaComponent},
-  { path: 'detalleorden/:id', component: DetalleordenComponent },
+  { path: 'detalleorden', component: DetalleordenComponent },
   { path: 'persona', component: PersonaComponent },
   { path: 'lista-persona', component: ListaPersonaComponent },
   { path: 'servicio-admin', component:ServicioAdminComponent},
--- src/app/detalleorden/detalleorden.component.ts
+++ src/app/detalleorden/detalleorden.component.ts
@@ -1,47 +1,54 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { detalleorden } from './detalleorden';
 import { DetalleordenService } from './detalleorden.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OrdenesService } from '../orden/ordenes.service';
+import { personaService } from '../persona.service';
+import { Persona } from '../modelo/persona';
 @Component({
   selector: 'app-detalleorden',
   templateUrl: './detalleorden.component.html',
   styleUrls: ['./detalleorden.component.css']
+  
 })
 //obtenerListadetalleorden
 export class DetalleordenComponent implements OnInit {
  
   //VARIABLES Y ARRAYS
-  detalleorden: detalleorden[];
-  detalleso:detalleorden = new detalleorden();
+  inputValue: string = '';
+  persona:Persona[];
+  personax:Persona=new Persona();
   currentDate: string;
  suan:number=16;
+ idorden:number;
+
  //FIN DE VARIABLES Y ARRAYS
-  constructor(private detalleordenService:DetalleordenService) {   const today = new Date();
+  constructor(private personaServicio:personaService,private ordenesService: OrdenesService,private router:Router,private route:ActivatedRoute) {   const today = new Date();
     const year = today.getFullYear();
     const month = ('0' + (today.getMonth() + 1)).slice(-2);
     const day = ('0' + today.getDate()).slice(-2);
     this.currentDate = `${year}-${month}-${day}`;}
 
   ngOnInit(): void {
-    this.obtenerdetalleorden();
-   
-    
-    
-
+    this.obtenerPersona();
   }
-  private obtenerdetalleorden(){
-    this.detalleordenService.obtenerListadetalleorden().subscribe(dato => {
-      this.detalleorden = dato;
+  obtenerPersona(){
+    this.personaServicio.obtenerListaPersona().subscribe(dato => {
+  this.persona=dato;
     });
-  }
-
   
-  guardardetalle(){
-    this.detalleordenService.registrarorden(this.detalleso).subscribe(dato =>{
-      console.log(dato);
-      this.obtenerdetalleorden();
+    }
 
-    },error => console.log(error));
-  }
+    buscarporcedula(){
+      const valor: string = this.inputValue;
+      this.personaServicio.obtenerPersonaPoridentificacion(valor).subscribe(dato => {
+        this.personax=dato;
+        this.persona.splice(0,this.persona.length);
+        this.persona.push(this.personax);
+          });
+         
+    }
+  
 
 
 }
