c4dcabe4f39a2913375692de103a984fd5c84249
CAB3ZON <marcospillcocontreras@gmail.com>
2023-07-25 23:17:10 -0500

    solucion a la eliminacion de items y ademas validacion a cantidades negativas
    xsxsxsx

--- src/app/detalleorden/detalleorden.component.ts
+++ src/app/detalleorden/detalleorden.component.ts
@@ -12,7 +12,6 @@ import { ItemService } from '../_services/item.service';
 import Swal from 'sweetalert2';
 import { orden } from '../modelo/orden';
 import { AllScriptServiceService } from '../all-script-service.service';
-import { AuthService } from '../_services/auth.service';
 @Component({
   selector: 'app-detalleorden',
   templateUrl: './detalleorden.component.html',
@@ -37,13 +36,13 @@ export class DetalleordenComponent implements OnInit {
   idorden: number;
   totalx: number = 0;
   maquinas: Maquina[];
-  servicios: Servicio[];
+  servicios: Servicio[]= [];
   listaItem: Item[] = [];
   cantidad_maq: { [key: string]: number } = {};
   cantidad_ser: { [key: string]: number } = {};
   idgenerado: number;
   //FIN DE VARIABLES Y ARRAYS
-  constructor(private auth:AuthService, private itemServicio: ItemService, private servicioServicio: ServicioService, private maquinaServicio: MaquinaService, private personaServicio: personaService, private ordenesService: OrdenesService, private router: Router, private route: ActivatedRoute, private AllScripts:
+  constructor(private itemServicio: ItemService, private servicioServicio: ServicioService, private maquinaServicio: MaquinaService, private personaServicio: personaService, private ordenesService: OrdenesService, private router: Router, private route: ActivatedRoute, private AllScripts:
     AllScriptServiceService) {
     const today = new Date();
     const year = today.getFullYear();
@@ -60,9 +59,26 @@ export class DetalleordenComponent implements OnInit {
     this.obtenerServicios();
     this.obtenersinOrdenes();
     
+    
 
   }
+
+  
+  
+
+  eliminaritem(id:number){
+   //aqui va service de item
+        this.itemServicio.eliminarItem(id).subscribe(dato => {
+          this.obtenersinOrdenes();
+          //this.obtenerEmpleados();   //pa que se actualize
+        })
+    }
+
+
+
+
   obtenerPersona() {
+  
     this.personaServicio.obtenerListaPersona().subscribe(dato => {
       this.personas = dato;
 
@@ -147,6 +163,10 @@ export class DetalleordenComponent implements OnInit {
   obtenerMaquinaria() {
     this.maquinaServicio.obtenerListaMaquinas().subscribe(dato => {
       this.maquinas = dato;
+      for (let i = 0; i < this.maquinas.length; i++) {
+        this.cantidad_maq[i] = 1;
+        
+      }
     });
   }
 
@@ -154,12 +174,23 @@ export class DetalleordenComponent implements OnInit {
   obtenerServicios() {
     this.servicioServicio.obtenerListaMaquinas().subscribe(dato => {
       this.servicios = dato;
+      for (let i = 0; i < this.servicios.length; i++) {
+        this.cantidad_ser[i] = 1;
+        
+      }
     });
 
   }
 
 
   crearItemMaquina(maquina: any, cantidad: number) {
+    if (cantidad<=0) {
+      Swal.fire('CANTIDAD INVALIDA', '', 'warning');
+
+    } else {
+
+    
+    //cdcsdfsdfdsf
     this.itemSelect = {} as Item;
     this.itemSelect.cantidad = cantidad;
     this.itemSelect.maquina = maquina;
@@ -169,11 +200,18 @@ export class DetalleordenComponent implements OnInit {
       this.obtenersinOrdenes();
 
     }, error => console.log(error));
-
+  }
   }
 
 
   crearItemServicio(servicio: Servicio, cantidad: number) {
+    if (cantidad<=0) {
+      Swal.fire('CANTIDAD INVALIDA', '', 'warning');
+
+    } else {
+       
+
+    //aqui va mensaje de error si es negativo o 0
     this.itemSelect = {} as Item;
     this.itemSelect.cantidad = cantidad;
     this.itemSelect.servicio = servicio;
@@ -183,7 +221,7 @@ export class DetalleordenComponent implements OnInit {
       this.obtenersinOrdenes();
 
     }, error => console.log(error));
-
+  }
   }
 
 
